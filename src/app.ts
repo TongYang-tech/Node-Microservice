50e5605f606913d7558043dc124b3eeae06734d9
Tong Yang <tongyang80@yahoo.com>
2026-04-12 22:19:28 -0500

    fix: update bruno directory and add middleware packages.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
 import express from "express";
 import route from "./route/index";
 import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import cors from "cors";
 
 const app = express();
 const basePath = "/placeholder";
 
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
+
 app.use(basePath, route);
 
 export default app;
